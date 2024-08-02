3550f36335b97983b68b61444b49c2097d532448
Rajat Kesharwani <rajat.sunny2@gmail.com>
2024-08-02 18:58:59 +0530

    fixed for gpt search page responsive

--- src/components/GPTSearch.js
+++ src/components/GPTSearch.js
@@ -6,7 +6,7 @@ import { BG_IMAGE } from '../utils/constants'
 const GPTSearch = () => {
   return (
     <div>
-        <div className='fixed -z-10'>
+        <div className='md:w-full fixed -z-10'>
             <img className="h-screen md:w-full object-cover" src={BG_IMAGE} alt='background' />
         </div>
         <GPTSearchBar />
