acff09ac31986a1912ecd001a51adc7e175bb085
Rajat Kesharwani <rajat.sunny2@gmail.com>
2024-08-02 17:39:56 +0530

    added error page

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,11 +3,13 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
 import Login from './Login'
 import Browse from './Browse'
+import NotFound from './NotFound'
 
 const Body = () => {
     const appRoutes = createBrowserRouter([
         { path: '/',        element: <Login /> },
-        { path: '/browse',  element: <Browse /> }
+        { path: '/browse',  element: <Browse /> },
+        { path: '*',         element: <NotFound />}
     ]);
 
     return (
---
+++ src/components/NotFound.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './Header'
+import { BG_IMAGE } from '../utils/constants'
+
+const NotFound = () => {
+  return (
+    <div>
+        <div>
+            <img className=" fixed -z-10 object-cover" src={BG_IMAGE} alt='background' />
+        </div>
+        <div>
+            <h1 className='px-60 py-80 text-3xl text-white'>404, NotFound!</h1>
+        </div>
+    </div>
+  )
+}
+
+export default NotFound
