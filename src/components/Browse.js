034c77560e7b67d2cbe650e47a53cf18718c4fb2
Rajat Kesharwani <rajat.sunny2@gmail.com>
2024-08-10 16:29:19 +0530

    revert browse changes

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -7,12 +7,10 @@ import useMovies from '../hooks/useMovies';
 import GPTSearch from './GPTSearch';
 import { useSelector } from 'react-redux';
 import JoyRide from './JoyRide';
-import VideoPlay from './VideoPlay';
 
 const Browse = () => {
 
     const showGPTSearch = useSelector((state) => state.gpt.showGPTSearch);
-    const showVideoPage = useSelector((state) => state.movies.showVideoPage);
 
     useMovies(MOVIES_TYPE[0]);
     useMovies(MOVIES_TYPE[1]);
